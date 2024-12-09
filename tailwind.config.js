030ba7d9a56d928ab6f689b41e869d714827b124
Nobodip Debnath <nobodipdebnath45@.com>
2024-12-09 21:30:47 +0600

    add all file

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme:{
+        fontFamily:{
+            Outfit:["Outfit", "sans-serif"],
+            Ovo:["Ovo", "serif"]
+        },
+        animation:{
+            spin_slow: 'spin 6s linear infinite'
+        }
+    }
+}
