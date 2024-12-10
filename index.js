c9080118fb97814c190674fb9eeb9f94855d504c
Nobodip Debnath <nobodipdebnath45@.com>
2024-12-11 00:40:48 +0600

    do all the work

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ function closeMenu(){
 
 window.addEventListener('scroll', ()=>{
     if(scrollY > 50){
-        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
-        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50')
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm','dark:bg-darkTheme', 'dark:shadow-white/20')
+        navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white', 'dark:bg-transparent')
     }
     else{
-        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm')
-        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50')
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm','dark:bg-darkTheme', 'dark:shadow-white/20')
+        navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', 'dark:border-white', 'dark:bg-transparent')
     }
 })
+// light mode in dark mode
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme:   dark)').matches)){
+    document.documentElement.classList.add('dark')
+} else{
+    document.documentElement.classList.remove('dark')
+}
+
+function toggleTheme(){
+    document.documentElement.classList.toggle('dark')
+    if(document.documentElement.classList.contains('dark')){
+        localStorage.theme = 'dark';
+    } else{
+        localStorage.theme = 'light';
+    }
+}
+
